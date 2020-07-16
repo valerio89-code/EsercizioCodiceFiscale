7215a732daa05ca3616b65a8f691c8e0b7ea38c7
Valerio Valdes <valerio.valdes89@outlook.com>
2020-07-16 12:42:17 +0200

    Add project files.

---
+++ EsercizioCodiceFiscale/CodiceFiscale.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace EsercizioCodiceFiscale
+{
+    partial class CodiceFiscale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CodiceFiscale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 707);
+            this.Name = "CodiceFiscale";
+            this.Text = "Calcolo codice fiscale";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
