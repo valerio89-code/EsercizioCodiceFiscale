3e7748abe52ea06aa5611f0375d457bd62bbb10a
Valerio Valdes <valerio.valdes89@outlook.com>
2020-07-17 13:03:24 +0200

    Versione 1.0

---
+++ EsercizioCodiceFiscale/Code/HelperCodiceFiscale.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioCodiceFiscale.Code
+{
+    /*
+ Assemini a474
+Cagliari b354
+Genova d969
+Iglesias e281
+Milano f205
+*/
+    static class HelperCodiceFiscale
+    {
+        public static Dictionary<string, string> CodiciComuni = new Dictionary<string, string>
+        {
+            {"assemini", "a474" },
+            {"cagliari", "b354" },
+            {"genova", "d969" },
+            {"iglesias", "e281" },
+            {"milano", "f205" },
+            {"portoscuso", "g922" }
+        };
+        public static string GetCodiceCatastale(string comune)
+        {
+            //if (CodiciComuni.ContainsKey(comune))
+            //{
+            //    return CodiciComuni[comune];
+            //}
+            //return null;
+
+            return CodiciComuni.ContainsKey(comune.ToLower()) ? CodiciComuni[comune.ToLower()] : null;
+        }
+        public static string GetCodiceCatastaleConSwitch(string comune)
+        {
+            switch (comune.ToLower())
+            {
+                case "cagliari":
+                    return "b354";
+                case "assemini":
+                    return "a474";
+                case "genova":
+                    return "d969";
+                case "iglesias":
+                    return "e281";
+                case "milano":
+                    return "f205";
+                default:
+                    return null;
+            }
+        }
+    }
+}
--- EsercizioCodiceFiscale/CodiceFiscale.cs
+++ EsercizioCodiceFiscale/CodiceFiscale.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EsercizioCodiceFiscale.Code;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +18,18 @@ namespace EsercizioCodiceFiscale
             InitializeComponent();
         }
 
-        private void btn_calcola_Click(object sender, EventArgs e)
+        private void Btn_calcola_Click(object sender, EventArgs e)
         {
+            var nome = tbx_nome.Text;
+            var cognome = tbx_cognome.Text;
+            var data = dataDiNascita.Value;
+            var sesso = rbtn_Maschio.Checked ? "m" : "f";
+            var comuneScelto = cmbx_comune.SelectedItem.ToString();
+            var codiceCatastale = HelperCodiceFiscale.GetCodiceCatastale(comuneScelto);
+            var cfCalculator = new Tecnosoftware.CodiceFiscale.CodiceFiscale();
+            lbl_codice.Text = cfCalculator.calcolaCF(cognome, nome, data, sesso, codiceCatastale);
 
         }
+
     }
 }
